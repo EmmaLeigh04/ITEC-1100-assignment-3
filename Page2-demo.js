9e3b067912259b4f56d4812c4a06f4d1e645dfb2
Emma Maxwell <emmamaxwell@cmail.carleton.ca>
2025-12-04 11:57:51 -0500

    bug fixes

--- Page2-demo.js
+++ Page2-demo.js
@@ -2,5 +2,5 @@ const gameButton = document.querySelector(".gameButton");
 
 gameButton.onclick = (event) => {
     event.preventDefault();
-    window.location.replace(`/game.html`);
+    window.location.replace(`game.html`);
 }
