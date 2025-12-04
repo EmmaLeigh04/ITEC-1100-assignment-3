995b696187e6a2f7abe47dfd9af50559f1418c83
Emma Maxwell <emmamaxwell@cmail.carleton.ca>
2025-12-04 11:56:42 -0500

    bug fixes

--- Page1.js
+++ Page1.js
@@ -2,5 +2,5 @@ const demoButton = document.querySelector(".demoButton");
 
 demoButton.onclick = (event) => {
     event.preventDefault();
-    window.location.replace(`/Page2-demo.html`);
+    window.location.replace(`Page2-demo.html`);
 }
